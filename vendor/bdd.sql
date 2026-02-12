d86d52d15a48029e1616fd7cb608c97ea70054f2
Alintsoa <alintsoarakotonarivo@gmail.com>
2026-02-12 12:05:05 +0300

    update bdd

--- vendor/bdd.sql
+++ vendor/bdd.sql
@@ -12,3 +12,19 @@ INSERT INTO users (email, username, password_hash)
 VALUES 
 ('alice@example.com', 'Alice', 'HASH_MDP1'),
 ('bob@example.com', 'Bob', 'HASH_MDP2');
+
+-- Table Catégorie
+CREATE TABLE categorie (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL
+);
+
+-- Table Objets
+CREATE TABLE objets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    description TEXT,
+    photo VARCHAR(255),
+    proprietaire INT NOT NULL,
+    FOREIGN KEY (proprietaire) REFERENCES users(id) ON DELETE CASCADE
+);
